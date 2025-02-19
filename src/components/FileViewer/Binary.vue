e3920a0bd770de611532204d2074977b829d3689
Michele Locati <michele@locati.it>
2025-02-19 17:58:46 +0100

    Display archive name in generic file viewer

--- src/components/FileViewer/Binary.vue
+++ src/components/FileViewer/Binary.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { computed } from 'vue';
-import { ZipFile } from '../../ZipArchive';
+import { ZipArchive, ZipEntry, ZipFile } from '../../ZipArchive';
 import { formatSize } from '../../Size';
 
 const props = defineProps<{
@@ -11,20 +11,35 @@ const size = computed<number>(() => {
     return props.zipFile.data.byteLength;
 });
 
+const zipArchive = computed<ZipArchive|null>(() => {
+    let entry : ZipEntry = props.zipFile;
+    while (entry.parent) {
+        entry = entry.parent;
+        if (entry instanceof ZipArchive) {
+            return entry;
+        }
+    }
+    return null;
+});
+
 const displaySize = computed<string>(() => formatSize(size.value));
 
 </script>
 
 <template>
-    <table class="table table-bordered">
+    <table class="table table-bordered w-auto">
         <tbody>
+            <tr v-if="zipArchive !== null">
+                <th>Archive</th>
+                <td><code>{{ zipArchive.zipFilename }}</code></td>
+            </tr>
             <tr>
                 <th>Name</th>
-                <td>{{ props.zipFile.name }}</td>
+                <td><code>{{ props.zipFile.name }}</code></td>
             </tr>
             <tr>
                 <th>Full Path</th>
-                <td>{{ props.zipFile.path }}</td>
+                <td><code>{{ props.zipFile.path }}</code></td>
             </tr>
             <tr>
                 <th>Size</th>
