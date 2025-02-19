b33c754b3b9fad1a7f411ad5cc83c9d95f2107ed
Michele Locati <michele@locati.it>
2025-02-19 10:43:18 +0100

    Add syntax highlight to file viewer

--- src/components/FileViewer/Text.vue
+++ src/components/FileViewer/Text.vue
@@ -3,6 +3,7 @@ import { computed, onMounted, watch } from 'vue';
 import { ZipFile } from '../../ZipArchive';
 import { getBeautifierFromFilename, type Beautifier } from '../../Beautifier';
 import { beautifierApplied, beautifierExists } from "./Data";
+import { getHighlightJsLanguageFromFilename } from '../../FileFormat';
 
 const props = defineProps<{
     zipFile: ZipFile,
@@ -20,6 +21,10 @@ const displayText = computed<string>(() => {
     return beautifier.value !== null && beautifierApplied.value ? beautifier.value(text.value) : text.value;
 });
 
+const highlightJsLanguage = computed<string>(() => {
+    return getHighlightJsLanguageFromFilename(props.zipFile.name);
+});
+
 watch(beautifier, (newBeautifier) => {
     beautifierExists.value = newBeautifier !== null;
 });
@@ -30,11 +35,5 @@ onMounted(() => {
 </script>
 
 <template>
-    <div>{{ displayText }}</div>
+    <highlightjs :autodetect="highlightJsLanguage === ''" :language="highlightJsLanguage" :code="displayText" />
 </template>
-
-<style lang="css" scoped>
-div {
-    white-space: pre-wrap;
-}
-</style>
